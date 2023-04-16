be4c9d2fc78c0ccc6980bd3b49e9265f6d67eb1c
wahyudi124 <wahyudiprasatia3@gmail.com>
2023-04-16 21:22:37 +0700

    tambahan blinking

--- fistcode.ino
+++ fistcode.ino
@@ -9,6 +9,9 @@ void loop(){
     delay(1000);    
     pinMode(14,LOW);
     if(digitalRead(14) == 0){
-        
+        digitalWrite(13,HIGH);
+        delay(1000);
+        digitalWrite(13,LOW);
+        delay(1000); 
     }
 }
