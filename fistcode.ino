c6c54b0008aec35a09bb49fd8b4f91b835c2aa0e
Wahyudi Abdullah <wahyudiprasatia3@gmail.com>
2023-04-17 21:38:04 +0700

    add blink
    add blink

--- fistcode.ino
+++ fistcode.ino
@@ -18,4 +18,10 @@ void loop(){
     delay(1000);
 //     digitalWrite(13,LOW);
 //     delay(1000);
+    if(digitalRead(14) == 0){
+        digitalWrite(13,HIGH);
+        delay(1000);
+        digitalWrite(13,LOW);
+        delay(1000); 
+    }
 }
